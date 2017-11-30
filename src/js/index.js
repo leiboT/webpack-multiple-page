db6d4aa0dae0425ed7e92e75a1cf66d9d63156fe
wangzhaolun <leibo_369@163.com>
2017-11-30 16:22:37 +0800

    add console

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../stylus/index.stylus';
 
 window.onload = function (){
+    console.log('项目源码:https://github.com/leiboT/webpack-multiple-page');
     document.addEventListener('touchmove', function (e) {
         e.preventDefault()
     });
@@ -9,7 +10,9 @@ window.onload = function (){
         pr = window.devicePixelRatio || 1,
         w = window.innerWidth,
         h = window.innerHeight,
+        // 偏移距离
         f = 90,
+        // 存储路径坐标
         q,
         m = Math,
         r = 0,
@@ -19,31 +22,39 @@ window.onload = function (){
     c.width = w*pr;
     c.height = h*pr;
     x.scale(pr, pr);
-    x.globalAlpha = 0.6;
-    function i(){
+    // 绘制图形的透明度
+    x.globalAlpha = 0.8;
+    //绘制 init
+    function init(){
         x.clearRect(0,0,w,h);
+        // 定义第一次和第二次绘制坐标
         q=[{x:0,y:h*.7+f},{x:0,y:h*.7-f}];
-        while(q[1].x<w+f) d(q[0], q[1])
+        // 只要绘制坐标X轴小于文档区域内宽度加上偏移量，一直调用绘制方法
+        while(q[1].x<w+f) draw(q[0], q[1])
     }
-    function d(i,j){
+    // 绘制方法 --路径绘制
+    function draw(i,j){
         x.beginPath();
         x.moveTo(i.x, i.y);
         x.lineTo(j.x, j.y);
+        // 生成第二次lineTo坐标
         let k = j.x + (z()*2-0.25)*f,
             n = y(j.y);
         x.lineTo(k, n);
         x.closePath();
+        // 16进制颜色递进生成
         r-=u/-50;
         x.fillStyle = '#'+(v(r)*127+128<<16 | v(r+u/3)*127+128<<8 | v(r+u/3*2)*127+128).toString(16);
         x.fill();
         q[0] = q[1];
         q[1] = {x:k,y:n}
     }
+    // 坐标y轴生成
     function y(p){
         let t = p + (z()*2-1.1)*f;
         return (t>h||t<0) ? y(p) : t
     }
-    document.onclick = i;
-    document.ontouchstart = i;
-    i();
+    document.onclick = init;
+    document.ontouchstart = init;
+    init();
 };
